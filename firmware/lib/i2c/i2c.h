ffecd8cd79001e43df5a205a9362e1eb8fd1b7c7
Joao <joaopedropinheiro@outlook.com>
2026-07-22 17:02:55 -0400

    start mpu config on main-firmware

--- firmware/lib/i2c/i2c.h
+++ firmware/lib/i2c/i2c.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 void i2c_init();
 void i2c_write_register(uint8_t addr, uint8_t reg, uint8_t data);
 uint8_t i2c_read_register(uint8_t addr, uint8_t reg);
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,14 +1,21 @@
 #include "led.h"
 #include "uart.h"
 #include "delay.h"
+#include "i2c.h"
+#include "mpu6500.h"
 #include <stm32f1xx.h>
 
 int main() {
 
     led_init();
     uart_init();
+    MPU6500 mpu_sensor = MPU6500(0x68, i2c_write_register, i2c_read_register);
+    bool mpu_check = mpu_sensor.init();
+    if (!mpu_check){
+        uart_send_string("MPU6500 não encontrado\r\n");
+    }
 
-    uart_send_string("Hello from STM32!\r\n");
+    uart_send_string("UART check...\r\n");
     // pisca-pisca para verificar que o STM32 está ligado
     while (1) {
         delay(100000);
